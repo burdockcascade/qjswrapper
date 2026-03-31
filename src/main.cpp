7050728170ffea13a71ceeb6a9d57bcdbc84172f
Peter Richmond <3448039+burdockcascade@users.noreply.github.com>
2026-03-31 08:58:47 +0100

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -1,230 +1,113 @@
-#include <any>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <memory>
-#include <functional>
-#include <expected>
-#include "quickjs.h"
 #include "raylib.h"
-
+#include "qjswrapper.hpp"
 
 namespace qjs {
 
-    template<typename T>
-    struct converter {
+    template<>
+    struct converter<Color> {
+        static Color get(JSContext* ctx, JSValueConst v) {
+            uint32_t r, g, b, a;
+            JSValue r_val = JS_GetPropertyStr(ctx, v, "r");
+            JSValue g_val = JS_GetPropertyStr(ctx, v, "g");
+            JSValue b_val = JS_GetPropertyStr(ctx, v, "b");
+            JSValue a_val = JS_GetPropertyStr(ctx, v, "a");
 
-        // --- FROM JS (Get from JS) ---
-        static T get(JSContext* ctx, JSValueConst v) {
-            if constexpr (std::integral<T>) {
-                if constexpr (std::is_same_v<T, bool>) {
-                    return JS_ToBool(ctx, v);
-                } else {
-                    int32_t val = 0;
-                    JS_ToInt32(ctx, &val, v);
-                    return static_cast<T>(val);
-                }
-            }
-            else if constexpr (std::floating_point<T>) {
-                double val = 0;
-                JS_ToFloat64(ctx, &val, v);
-                return static_cast<T>(val);
-            }
-            else if constexpr (std::is_convertible_v<T, std::string>) {
-                size_t len;
-                const char* str = JS_ToCStringLen(ctx, &len, v);
-                std::string s(str ? str : "", len);
-                JS_FreeCString(ctx, str);
-                return s;
-            }
-            // Raylib specific branch
-            else if constexpr (std::is_same_v<T, Color>) {
-                uint32_t r, g, b, a;
-                JSValue r_val = JS_GetPropertyStr(ctx, v, "r");
-                JSValue g_val = JS_GetPropertyStr(ctx, v, "g");
-                JSValue b_val = JS_GetPropertyStr(ctx, v, "b");
-                JSValue a_val = JS_GetPropertyStr(ctx, v, "a");
+            JS_ToUint32(ctx, &r, r_val);
+            JS_ToUint32(ctx, &g, g_val);
+            JS_ToUint32(ctx, &b, b_val);
+            JS_ToUint32(ctx, &a, a_val);
 
-                JS_ToUint32(ctx, &r, r_val);
-                JS_ToUint32(ctx, &g, g_val);
-                JS_ToUint32(ctx, &b, b_val);
-                JS_ToUint32(ctx, &a, a_val);
+            JS_FreeValue(ctx, r_val); JS_FreeValue(ctx, g_val);
+            JS_FreeValue(ctx, b_val); JS_FreeValue(ctx, a_val);
 
-                JS_FreeValue(ctx, r_val); JS_FreeValue(ctx, g_val);
-                JS_FreeValue(ctx, b_val); JS_FreeValue(ctx, a_val);
-
-                return Color{static_cast<unsigned char>(r), static_cast<unsigned char>(g), static_cast<unsigned char>(b), static_cast<unsigned char>(a)};
-            }
-            else if constexpr (std::is_same_v<T, Vector2>) {
-                double x, y;
-                JSValue x_val = JS_GetPropertyStr(ctx, v, "x");
-                JSValue y_val = JS_GetPropertyStr(ctx, v, "y");
-                JS_ToFloat64(ctx, &x, x_val);
-                JS_ToFloat64(ctx, &y, y_val);
-                JS_FreeValue(ctx, x_val); JS_FreeValue(ctx, y_val);
-                return Vector2{static_cast<float>(x), static_cast<float>(y)};
-            } else {
-                static_assert(sizeof(T) == 0, "Unsupported type for QJS conversion");
-            }
+            return Color{static_cast<unsigned char>(r), static_cast<unsigned char>(g), static_cast<unsigned char>(b), static_cast<unsigned char>(a)};
         }
-
-        // --- TO JS (Put into JS) ---
-        static JSValue put(JSContext* ctx, const T& val) {
-            if constexpr (std::integral<T>) {
-                if constexpr (std::is_same_v<T, bool>) {
-                    return JS_NewBool(ctx, val);
-                } else {
-                    return JS_NewInt32(ctx, static_cast<int32_t>(val));
-                }
-            }
-            else if constexpr (std::floating_point<T>) {
-                return JS_NewFloat64(ctx, static_cast<double>(val));
-            }
-            else if constexpr (std::is_convertible_v<T, std::string_view>) {
-                return JS_NewStringLen(ctx, val.data(), val.size());
-            }
-            // Raylib specific branch
-            else if constexpr (std::is_same_v<T, Color>) {
-                JSValue obj = JS_NewObject(ctx);
-                JS_SetPropertyStr(ctx, obj, "r", JS_NewInt32(ctx, val.r));
-                JS_SetPropertyStr(ctx, obj, "g", JS_NewInt32(ctx, val.g));
-                JS_SetPropertyStr(ctx, obj, "b", JS_NewInt32(ctx, val.b));
-                JS_SetPropertyStr(ctx, obj, "a", JS_NewInt32(ctx, val.a));
-                return obj;
-            }
-            else if constexpr (std::is_same_v<T, Vector2>) {
-                JSValue obj = JS_NewObject(ctx);
-                JS_SetPropertyStr(ctx, obj, "x", JS_NewFloat64(ctx, val.x));
-                JS_SetPropertyStr(ctx, obj, "y", JS_NewFloat64(ctx, val.y));
-                return obj;
-            }
-            else {
-                return JS_UNDEFINED;
-            }
+        static JSValue put(JSContext* ctx, Color val) {
+            JSValue obj = JS_NewObject(ctx);
+            JS_SetPropertyStr(ctx, obj, "r", JS_NewInt32(ctx, val.r));
+            JS_SetPropertyStr(ctx, obj, "g", JS_NewInt32(ctx, val.g));
+            JS_SetPropertyStr(ctx, obj, "b", JS_NewInt32(ctx, val.b));
+            JS_SetPropertyStr(ctx, obj, "a", JS_NewInt32(ctx, val.a));
+            return obj;
         }
     };
 
-    // --- The Engine ---
-    class Engine {
-    public:
-        Engine() : rt_(JS_NewRuntime()), ctx_(JS_NewContext(rt_.get())) {
-            global_obj_ = JS_GetGlobalObject(ctx_.get());
+    template<>
+    struct converter<Vector2> {
+        static Vector2 get(JSContext* ctx, JSValueConst v) {
+            double x, y;
+            JSValue x_val = JS_GetPropertyStr(ctx, v, "x");
+            JSValue y_val = JS_GetPropertyStr(ctx, v, "y");
+            JS_ToFloat64(ctx, &x, x_val);
+            JS_ToFloat64(ctx, &y, y_val);
+            JS_FreeValue(ctx, x_val); JS_FreeValue(ctx, y_val);
+            return Vector2{static_cast<float>(x), static_cast<float>(y)};
         }
-
-        ~Engine() {
-            JS_FreeValue(ctx_.get(), global_obj_);
+        static JSValue put(JSContext* ctx, Vector2 val) {
+            JSValue obj = JS_NewObject(ctx);
+            JS_SetPropertyStr(ctx, obj, "x", JS_NewFloat64(ctx, val.x));
+            JS_SetPropertyStr(ctx, obj, "y", JS_NewFloat64(ctx, val.y));
+            return obj;
         }
-
-        std::expected<std::string, std::string> eval(std::string_view code) {
-            const JSValue val = JS_Eval(ctx_.get(), code.data(), code.size(), "input.js", JS_EVAL_TYPE_GLOBAL);
-            if (JS_IsException(val)) {
-                const JSValue exception = JS_GetException(ctx_.get());
-                std::string err = converter<std::string>::get(ctx_.get(), exception);
-                JS_FreeValue(ctx_.get(), exception);
-                return std::unexpected(err);
-            }
-            std::string res = converter<std::string>::get(ctx_.get(), val);
-            JS_FreeValue(ctx_.get(), val);
-            return res;
-        }
-
-        template<typename Func>
-        void bind(std::string_view name, Func&& func) {
-            // We use a helper to deduce the function signature from the lambda's operator()
-            bind_impl(name, std::function(std::forward<Func>(func)));
-        }
-
-    private:
-        // Helper to perform the actual call
-        template<typename R, typename... Args, size_t... I>
-        static JSValue invoke_helper(JSContext* ctx, std::function<R(Args...)>& f, JSValueConst* argv, std::index_sequence<I...>) {
-            if constexpr (std::is_void_v<R>) {
-                f(converter<std::decay_t<Args>>::get(ctx, argv[I])...);
-                return JS_UNDEFINED;
-            } else {
-                return converter<R>::put(ctx, f(converter<std::decay_t<Args>>::get(ctx, argv[I])...));
-            }
-        }
-
-        template<typename R, typename... Args>
-        void bind_impl(std::string_view name, std::function<R(Args...)> func) {
-            // 1. Wrap the function in std::any and put it in a unique_ptr.
-            // unique_ptr ensures the memory address of the function is stable
-            // even if the vector 'functions_' grows and reallocates.
-            auto storage = std::make_unique<std::any>(std::make_any<std::function<R(Args...)>>(std::move(func)));
-
-            // Get the raw pointer to the std::function inside the std::any
-            auto* f_ptr = std::any_cast<std::function<R(Args...)>>(storage.get());
-
-            // Store the unique_ptr in our vector to keep it alive
-            functions_.push_back(std::move(storage));
-
-            // 2. Pass the address of the function to JS as a 64-bit integer
-            JSValue data = JS_NewInt64(ctx_.get(), reinterpret_cast<int64_t>(f_ptr));
-
-            // 3. The Trampoline (C-style callback)
-            auto trampoline = [](JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv, int magic, JSValue* data) -> JSValue {
-                int64_t ptr_addr;
-                JS_ToInt64(ctx, &ptr_addr, data[0]);
-
-                // Cast the memory address back to our function type
-                auto* f = reinterpret_cast<std::function<R(Args...)>*>(ptr_addr);
-
-                if (argc < sizeof...(Args)) {
-                    return JS_ThrowTypeError(ctx, "Expected %zu arguments, got %d", sizeof...(Args), argc);
-                }
-
-                return invoke_helper(ctx, *f, argv, std::make_index_sequence<sizeof...(Args)>{});
-            };
-
-            // 4. Register with QuickJS
-            JSValue js_func = JS_NewCFunctionData(ctx_.get(), trampoline, sizeof...(Args), 0, 1, &data);
-            JS_SetPropertyStr(ctx_.get(), global_obj_, name.data(), js_func);
-
-            // Clean up the temporary JS wrapper for the pointer
-            JS_FreeValue(ctx_.get(), data);
-        }
-
-        struct RuntimeDeleter { void operator()(JSRuntime* rt) const { JS_FreeRuntime(rt); } };
-        struct ContextDeleter { void operator()(JSContext* ctx) const { JS_FreeContext(ctx); } };
-
-        std::unique_ptr<JSRuntime, RuntimeDeleter> rt_;
-        std::unique_ptr<JSContext, ContextDeleter> ctx_;
-        JSValue global_obj_{};
-        std::vector<std::unique_ptr<std::any>> functions_;
     };
 
 }
+
 
 int cpp_add(int a, int b) {
     return a + b;
 }
 
-// --- Usage ---
-int main() {
-    InitWindow(800, 450, "Raylib + QuickJS");
+int run_math() {
     qjs::Engine engine;
 
-    // Bind DrawCircle(int centerX, int centerY, float radius, Color color)
-    engine.bind("drawCircle", [](int x, int y, float radius, Color color) {
-        DrawCircle(x, y, radius, color);
+    // Bind any C++ lambda
+    engine.bind("add", [](int a, int b) { return a + b; });
+
+    // Evaluate and get result (or error)
+    auto res = engine.eval("add(5, 10)");
+
+    if (res) {
+        std::cout << "Result: " << *res << "\n";
+    } else {
+        std::cerr << "JS Error: " << res.error() << "\n";
+    }
+}
+
+int run_raylib() {
+    // 1. Setup Raylib
+    InitWindow(800, 450, "QuickJS + Raylib File Loader");
+    SetTargetFPS(60);
+
+    qjs::Engine engine;
+
+    // 2. Bind the Raylib functions we need
+    engine.bind("clearBackground", [](Color c) { ClearBackground(c); });
+    engine.bind("drawCircle", [](int x, int y, float r, Color c) { DrawCircle(x, y, r, c); });
+    engine.bind("drawText", [](std::string text, int x, int y, int size, Color c) {
+        DrawText(text.c_str(), x, y, size, c);
     });
 
-    // Bind ClearBackground(Color color)
-    engine.bind("clearBackground", [](Color color) {
-        ClearBackground(color);
-    });
-
+    // 3. Main Game Loop
     while (!WindowShouldClose()) {
         BeginDrawing();
 
-        // You could run a script here, or better yet, call a JS 'update' function
-        engine.eval("clearBackground({r: 20, g: 20, b: 20, a: 255});");
-        engine.eval("drawCircle(400, 225, 50, {r: 255, g: 0, b: 0, a: 255});");
+        // Execute the external script every frame
+        // Note: In a real game, you'd likely load this once and call a JS 'update' function
+        auto result = engine.run_file(R"(C:\workspace\c\qjswrapper\src\game.js)");
+
+        if (!result) {
+            DrawText(result.error().c_str(), 10, 10, 20, RED);
+        }
 
         EndDrawing();
     }
 
     CloseWindow();
     return 0;
+}
+
+// --- Usage ---
+int main() {
+    run_raylib();
 }
---
+++ src/qjswrapper.hpp
@@ -0,0 +1,179 @@
+#pragma once
+
+#include <any>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <expected>
+#include <fstream>
+#include <sstream>
+#include <filesystem>
+#include "quickjs.h"
+
+namespace qjs {
+
+    template<typename T>
+    struct converter {
+
+        // --- FROM JS (Get from JS) ---
+        static T get(JSContext* ctx, JSValueConst v) {
+            if constexpr (std::integral<T>) {
+                if constexpr (std::is_same_v<T, bool>) {
+                    return JS_ToBool(ctx, v);
+                } else {
+                    int32_t val = 0;
+                    JS_ToInt32(ctx, &val, v);
+                    return static_cast<T>(val);
+                }
+            }
+            else if constexpr (std::floating_point<T>) {
+                double val = 0;
+                JS_ToFloat64(ctx, &val, v);
+                return static_cast<T>(val);
+            }
+            else if constexpr (std::is_convertible_v<T, std::string>) {
+                size_t len;
+                const char* str = JS_ToCStringLen(ctx, &len, v);
+                std::string s(str ? str : "", len);
+                JS_FreeCString(ctx, str);
+                return s;
+            }
+            else {
+                static_assert(sizeof(T) == 0, "Unsupported type for QJS conversion");
+            }
+            return T{};
+        }
+
+        // --- TO JS (Put into JS) ---
+        static JSValue put(JSContext* ctx, const T& val) {
+            if constexpr (std::integral<T>) {
+                if constexpr (std::is_same_v<T, bool>) {
+                    return JS_NewBool(ctx, val);
+                } else {
+                    return JS_NewInt32(ctx, static_cast<int32_t>(val));
+                }
+            }
+            else if constexpr (std::floating_point<T>) {
+                return JS_NewFloat64(ctx, static_cast<double>(val));
+            }
+            else if constexpr (std::is_convertible_v<T, std::string_view>) {
+                return JS_NewStringLen(ctx, val.data(), val.size());
+            }
+            else {
+                return JS_UNDEFINED;
+            }
+        }
+    };
+
+    // --- The Engine ---
+    class Engine {
+    public:
+        Engine() : rt(JS_NewRuntime()), ctx(JS_NewContext(rt.get())) {
+            global_obj = JS_GetGlobalObject(ctx.get());
+        }
+
+        ~Engine() {
+            JS_FreeValue(ctx.get(), global_obj);
+        }
+
+        std::expected<std::string, std::string> eval(std::string_view code, std::string_view filename = "input.js") {
+            // JS_Eval takes a null-terminated string for the filename
+            JSValue val = JS_Eval(ctx.get(), code.data(), code.size(), filename.data(), JS_EVAL_TYPE_GLOBAL);
+
+            if (JS_IsException(val)) {
+                JSValue exception = JS_GetException(ctx.get());
+                std::string err_msg = converter<std::string>::get(ctx.get(), exception);
+                JS_FreeValue(ctx.get(), exception);
+                return std::unexpected(err_msg);
+            }
+
+            std::string result = converter<std::string>::get(ctx.get(), val);
+            JS_FreeValue(ctx.get(), val);
+            return result;
+        }
+
+        std::expected<std::string, std::string> run_file(const std::filesystem::path& path) {
+            std::ifstream file(path);
+
+            if (!file.is_open()) {
+                return std::unexpected("Could not open file: " + path.string());
+            }
+
+            // Efficiently read the entire file into a string
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            std::string code = buffer.str();
+
+            // Use the filename as the 'source' for better error reporting in JS
+            return eval(code, path.filename().string());
+        }
+
+        template<typename Func>
+        void bind(std::string_view name, Func&& func) {
+            // We use a helper to deduce the function signature from the lambda's operator()
+            bind_impl(name, std::function(std::forward<Func>(func)));
+        }
+
+    private:
+        // Helper to perform the actual call
+        template<typename R, typename... Args, size_t... I>
+        static JSValue invoke_helper(JSContext* ctx, std::function<R(Args...)>& f, JSValueConst* argv, std::index_sequence<I...>) {
+            if constexpr (std::is_void_v<R>) {
+                f(converter<std::decay_t<Args>>::get(ctx, argv[I])...);
+                return JS_UNDEFINED;
+            } else {
+                return converter<R>::put(ctx, f(converter<std::decay_t<Args>>::get(ctx, argv[I])...));
+            }
+        }
+
+        template<typename R, typename... Args>
+        void bind_impl(std::string_view name, std::function<R(Args...)> func) {
+            // 1. Wrap the function in std::any and put it in a unique_ptr.
+            // unique_ptr ensures the memory address of the function is stable
+            // even if the vector 'functions_' grows and reallocates.
+            auto storage = std::make_unique<std::any>(std::make_any<std::function<R(Args...)>>(std::move(func)));
+
+            // Get the raw pointer to the std::function inside the std::any
+            auto* f_ptr = std::any_cast<std::function<R(Args...)>>(storage.get());
+
+            // Store the unique_ptr in our vector to keep it alive
+            functions.push_back(std::move(storage));
+
+            // 2. Pass the address of the function to JS as a 64-bit integer
+            JSValue data = JS_NewInt64(ctx.get(), reinterpret_cast<int64_t>(f_ptr));
+
+            // 3. The Trampoline (C-style callback)
+            auto trampoline = [](JSContext* ctx, JSValueConst this_val, int argc, JSValueConst* argv, int magic, JSValue* data) -> JSValue {
+                int64_t ptr_addr;
+                JS_ToInt64(ctx, &ptr_addr, data[0]);
+
+                // Cast the memory address back to our function type
+                auto* f = reinterpret_cast<std::function<R(Args...)>*>(ptr_addr);
+
+                if (argc < sizeof...(Args)) {
+                    return JS_ThrowTypeError(ctx, "Expected %zu arguments, got %d", sizeof...(Args), argc);
+                }
+
+                return invoke_helper(ctx, *f, argv, std::make_index_sequence<sizeof...(Args)>{});
+            };
+
+            // 4. Register with QuickJS
+            JSValue js_func = JS_NewCFunctionData(ctx.get(), trampoline, sizeof...(Args), 0, 1, &data);
+            JS_SetPropertyStr(ctx.get(), global_obj, name.data(), js_func);
+
+            // Clean up the temporary JS wrapper for the pointer
+            JS_FreeValue(ctx.get(), data);
+        }
+
+        struct RuntimeDeleter { void operator()(JSRuntime* rt) const { JS_FreeRuntime(rt); } };
+        struct ContextDeleter { void operator()(JSContext* ctx) const { JS_FreeContext(ctx); } };
+
+        std::unique_ptr<JSRuntime, RuntimeDeleter> rt;
+        std::unique_ptr<JSContext, ContextDeleter> ctx;
+        JSValue global_obj{};
+        std::vector<std::unique_ptr<std::any>> functions;
+    };
+
+}
